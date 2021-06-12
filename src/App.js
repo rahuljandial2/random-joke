b46f00bc175831101601375ed70cda8a4ee7f97f
rahuljandial2 <61629739+rahuljandial2@users.noreply.github.com>
2021-06-12 15:47:32 +0530

    Created a react project which renders a joke using api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component, useState } from 'react';
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      joke: "",
+      punchline: ""
+    };
+  }
+  getJoke = () => {
+    this.setState({
+      joke: "Loading.......",
+      punchline: "Please, Wait!!!"
+    });
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          joke: data.setup,
+          punchline: data.punchline
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          joke: "Something went wrong.....",
+          punchline: "Try again later!!!"
+        });
+      })
+  }
+  componentDidMount() {
+    this.getJoke();
+  }
+  render() {
+    return (
+      <div className="App">
+        <section className="App-container">
+          <h1>{this.state.joke}</h1>
+          <h2>{this.state.punchline}</h2>
+          <Button className="App-btn" variant="contained" color="primary" onClick={this.getJoke}>
+            Get Joke
+          </Button>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
